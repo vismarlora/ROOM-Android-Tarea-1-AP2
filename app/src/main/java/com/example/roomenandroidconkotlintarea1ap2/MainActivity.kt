b9e4cbc4133138203c6cdfc360e98690cc12da10
vismarlora <vismar_lora@ucne.edu.do>
2022-04-29 19:58:40 -0400

    Creando el Repositorio

---
+++ app/src/main/java/com/example/roomenandroidconkotlintarea1ap2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.roomenandroidconkotlintarea1ap2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
