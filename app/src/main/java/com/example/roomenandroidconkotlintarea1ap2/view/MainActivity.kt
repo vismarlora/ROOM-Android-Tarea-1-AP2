82a0f7c473627ac65c9d899fced9a77476d1fb42
vismarlora <vismar_lora@ucne.edu.do>
2022-04-29 23:39:02 -0400

    Conectando el ViewModel con el MainActivity

--- app/src/main/java/com/example/roomenandroidconkotlintarea1ap2/view/MainActivity.kt
+++ app/src/main/java/com/example/roomenandroidconkotlintarea1ap2/view/MainActivity.kt
@@ -2,13 +2,29 @@ package com.example.roomenandroidconkotlintarea1ap2.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import com.example.roomenandroidconkotlintarea1ap2.databinding.ActivityMainBinding
+import com.example.roomenandroidconkotlintarea1ap2.model.QuoteModel
+import com.example.roomenandroidconkotlintarea1ap2.viewModel.quoteViewModel
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding:ActivityMainBinding
+
+    private val quoteViewModel : quoteViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        quoteViewModel.quoteModel.observe(this, Observer {currentQuote ->
+            binding.tvQuote.text = currentQuote.quote
+            binding.tvAuthor.text = currentQuote.author
+
+        })
+
+        binding.viewContainer.setOnClickListener{ quoteViewModel.randomQuote() }
     }
 }
